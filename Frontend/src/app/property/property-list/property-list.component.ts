e308e87f6fa2c947cd3e091d940639988d018e60
Danilo Cavalcante <danilo.cavalcante@adm.unip.br>
2023-05-23 16:44:37 -0300

    adding housing service

--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -1,39 +1,29 @@
 import { Component } from '@angular/core';
+import { HousingService } from 'src/app/services/housing.service';
+
 
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
   styleUrls: ['./property-list.component.css']
 })
+
 export class PropertyListComponent {
 
-  Properties: Array<any> = [{
-    "Id": 1,
-    "Name": "House",
-    "Type": "House",
-    "Price": 120000
-  },
-  {
-    "Id": 2,
-    "Name": "Villa",
-    "Type": "Villa",
-    "Price": 120000
-  },
-  {
-    "Id": 3,
-    "Name": "Apartment",
-    "Type": "Apartment",
-    "Price": 120000
-  },
-  {
-    "Id": 4,
-    "Name": "Farm",
-    "Type": "Farm",
-    "Price": 120000
-  }]
+  Properties: any;
 
-  constructor(){
+  constructor(private housingSerivce: HousingService){
 
   }
 
+  ngOnInit():void {
+    this.housingSerivce.getAllProperties().subscribe(
+      data => {
+        this.Properties = data;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
 }
